c7617ec3018f98fe9daa21838e9807e566ed7490
KarineHoubrix <k.houbrix@ludus-academie.com>
2018-03-13 11:35:52 +0100

    13/03/2018 : Karine Houbrix : Rajout de commentaire inutile dans le fichier projet_test_unreal.cpp

--- Source/projet_test_unreal/projet_test_unreal.cpp
+++ Source/projet_test_unreal/projet_test_unreal.cpp
@@ -3,4 +3,6 @@
 #include "projet_test_unreal.h"
 #include "Modules/ModuleManager.h"
 
+//Ceci est un test, je répète, ceci est un test 
+
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, projet_test_unreal, "projet_test_unreal" );
